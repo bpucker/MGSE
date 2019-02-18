03e8a228658ed37b5583aba19704e29ad5b69156
Boas Pucker <36674497+bpucker@users.noreply.github.com>
2019-02-18 15:11:41 +0100

    Add files via upload

---
+++ construct_cov_file.py
@@ -0,0 +1,48 @@
+### Boas Pucker ###
+### bpucker@cebitec.uni-bielefeld.de ###
+### v0.2 ###
+
+__usage__ = """
+					python construct_cov_file.py\n
+					--in <BAM_FILE>
+					--out <OUTPUT_FILE>
+					
+					--bam_is_sorted <PREVENTS_EXTRA_SORTING_OF_BAM_FILE>
+					
+					feature requests and bug reports: bpucker@cebitec.uni-bielefeld.de
+					"""
+
+import os, sys
+
+# --- end of imports --- #
+
+def main( arguments ):
+	
+	bam_file = arguments[ arguments.index( '--in' )+1 ]
+	output_file = arguments[ arguments.index( '--out' )+1 ]
+		
+	samtools = "samtools"
+	bedtools = "genomeCoverageBed"
+	
+	if '--bam_is_sorted' in arguments:
+		sorted_bam_file = bam_file
+	else:
+		print "sorting BAM file ..."
+		sorted_bam_file = output_file + "_sorted.bam"
+		cmd = samtools + " sort -m 5000000000 --threads 8 " + bam_file + " > " + sorted_bam_file
+		os.popen( cmd )
+	
+	# --- calculate read coverage depth per position --- #
+	print "calculating coverage per position ...."
+	cmd = bedtools + " -d -split -ibam " + sorted_bam_file + " > " + output_file
+	os.popen( cmd )
+
+
+if __name__ == '__main__':
+	
+	if '--in' in sys.argv and '--out' in sys.argv:
+		main( sys.argv )
+	else:
+		sys.exit( __usage__ )
+	
+	print "all done!"
